ec853b8777aed1c02ddc3e2834cda49b508e86c3
Juan-Pablo Velez <jpvelez@gmail.com>
2015-11-12 22:49:19 -0500

    Bootstrap development environment on local or remote

---
+++ setup.sh
@@ -0,0 +1,12 @@
+if [[ $OSTYPE == 'linux-gnu' ]]; then
+   wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.0-Linux-x86_64.sh
+   bash Anaconda2-2.4.0-Linux-x86_64.sh
+fi
+conda create --name all-roads-meet python=2.7
+source activate all-roads-meet
+if [[ $OSTYPE == 'darwin14' ]]; then
+   brew install protobuf
+elif [[ $OSTYPE == 'linux-gnu' ]]; then
+   sudo aptitude install build-essential python-devel protobuf-compiler libprotobuf-dev
+fi
+pip install imposm.parser # Needs python 2.7
