85bce719bef47742334bfad4838bbe405a1b89f0
Juan-Pablo Velez <jpvelez@gmail.com>
2015-11-12 22:55:51 -0500

    Fix bug in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 if [[ $OSTYPE == 'linux-gnu' ]]; then
-   wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.0-Linux-x86_64.sh
-   bash Anaconda2-2.4.0-Linux-x86_64.sh
+   curl https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.0-Linux-x86_64.sh > anaconda
+   bash anaconda
 fi
 conda create --name all-roads-meet python=2.7
 source activate all-roads-meet
