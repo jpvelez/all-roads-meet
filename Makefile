27d15b49c5a36bc3d6200fdf38b35aec1380adae
Juan-Pablo Velez <jpvelez@gmail.com>
2015-11-25 05:15:30 +0000

    Extract NYC coordinate points from NYC Metro PBF file

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ data/nyc.osm.pbf:
 	curl https://s3.amazonaws.com/metro-extracts.mapzen.com/new-york_new-york.osm.pbf > $@
 
 # Extract road features out of NYC OSM data.
-parsed_data: data/nyc.osm.pbf src/parse_osm_data.py
-	python src/parse_osm_data.py $<
+data/nyc_intersections.tsv: data/nyc.osm.pbf src/parse_osm_data.py
+	python src/parse_osm_data.py $< $@
 
 # Test client for adjacency-list implementation of undirected graph.
 test-graph:
--- src/parse_osm_data.py
+++ src/parse_osm_data.py
@@ -1,21 +1,33 @@
 import sys
+import csv
 from imposm.parser import OSMParser
 
 class RoadExtractor():
+    intersection_coords = []
     road_ways = []
+
+    def coords_callback(self, coords):
+        for osmid, lat, lon in coords:
+            self.intersection_coords.append((osmid, lat, lon))
 
     def ways_callback(self, ways):
         for osmid, tags, refs in ways:
             if 'highway' in tags:
                 self.road_ways.append((osmid, tags, refs))
 
+# Parse intersection coordinates and road ways from OSM data input.
 extract = RoadExtractor()
-parser = OSMParser(ways_callback=extract.ways_callback)
-
+parser = OSMParser(coords_callback=extract.coords_callback, ways_callback=extract.ways_callback)
 parser.parse(sys.argv[1])
 
+# Print out features for inspection.
+# for ix, road in enumerate(extract.intersection_coords):
+#     print road
+#     if ix == 30:
+#         break
 
-for ix, road in enumerate(extract.road_ways):
-    print road
-    if ix == 30:
-        break
+# Save intersections to disk.
+writer = csv.DictWriter(open(sys.argv[2], 'w'), fieldnames=['osmid','lat','lon'])
+writer.writeheader()
+for osmid, lat, lon in extract.intersection_coords:
+    writer.writerow({'osmid': osmid, 'lat': lat, 'lon': lon})
