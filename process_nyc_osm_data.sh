d305908146fec4b034794912da8f258a4dfe7545
Juan-Pablo Velez <jpvelez@gmail.com>
2015-11-24 22:04:49 -0500

    Move code to src dir

--- process_nyc_osm_data.sh
+++ process_nyc_osm_data.sh
@@ -3,4 +3,4 @@ if [ ! -f $NYC_OSM ]; then
     curl https://s3.amazonaws.com/metro-extracts.mapzen.com/new-york_new-york.osm.pbf > $NYC_OSM
 fi
 
-python parse_osm_data.py $NYC_OSM
+python src/parse_osm_data.py $NYC_OSM
