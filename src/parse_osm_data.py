7fceb6cc51e8c706a2743d65f0ed8318245e52db
Juan-Pablo Velez <jpvelez@gmail.com>
2015-11-24 22:23:42 -0500

    Fix spacing in parse script

--- src/parse_osm_data.py
+++ src/parse_osm_data.py
@@ -2,16 +2,16 @@ import sys
 from imposm.parser import OSMParser
 
 class RoadExtractor():
-  road_ways = []
+    road_ways = []
 
-  def ways_callback(self, ways):
-    for osmid, tags, refs in ways:
-      if 'highway' in tags:
-    	self.road_ways.append((osmid, tags, refs))
+    def ways_callback(self, ways):
+        for osmid, tags, refs in ways:
+            if 'highway' in tags:
+                self.road_ways.append((osmid, tags, refs))
 
 extract = RoadExtractor()
 parser = OSMParser(ways_callback=extract.ways_callback)
 
 parser.parse(sys.argv[1])
 
-print extract.road_ways 
+print extract.road_ways
